2e138ee8674e815bbed0b3a9adace7548901207a
Jerozzzzzzzzzzzz <jerossas@outlook.com>
2026-03-01 16:32:59 -0500

    refactor(task-domain): remove unnecessary instance variable from Status enum to use built-in functionality

--- domain/src/main/java/com/dunnwr/cleantaskmanager/domain/Status.java
+++ domain/src/main/java/com/dunnwr/cleantaskmanager/domain/Status.java
@@ -2,15 +2,5 @@ package com.dunnwr.cleantaskmanager.domain;
 
 public enum Status {
 
-    PENDING("pending"), COMPLETED("completed");
-
-    private final String status;
-
-    Status(String status){
-        this.status = status;
-    }
-
-    public String getStatus(){
-        return this.status;
-    }
+    PENDING, COMPLETED;
 }
