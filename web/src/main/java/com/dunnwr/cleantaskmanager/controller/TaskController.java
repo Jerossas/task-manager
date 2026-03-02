1a4a405db10041b4e0b93f76d649f59bc233ffcb
Jerozzzzzzzzzzzz <jerossas@outlook.com>
2026-03-01 21:09:26 -0500

    feat(web): create rest controller and add functionality to create a new task using HTTP

---
+++ web/src/main/java/com/dunnwr/cleantaskmanager/controller/TaskController.java
@@ -0,0 +1,46 @@
+package com.dunnwr.cleantaskmanager.controller;
+
+import com.dunnwr.cleantaskmanager.domain.Task;
+import com.dunnwr.cleantaskmanager.dto.CreateTaskRequest;
+import com.dunnwr.cleantaskmanager.dto.TaskResponse;
+import com.dunnwr.cleantaskmanager.usecases.UseCase;
+import com.dunnwr.cleantaskmanager.usecases.commands.CreateTaskCommand;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+
+    private final UseCase<CreateTaskCommand, Task> createTaskUseCase;
+
+    public TaskController(UseCase<CreateTaskCommand, Task> createTaskUseCase){
+        this.createTaskUseCase = createTaskUseCase;
+    }
+
+    @PostMapping
+    public ResponseEntity<TaskResponse> createTask(@RequestBody CreateTaskRequest request){
+
+        CreateTaskCommand command = new CreateTaskCommand(
+                request.title(),
+                request.description(),
+                request.dueDate()
+        );
+
+        Task createdTask = createTaskUseCase.execute(command);
+
+        TaskResponse response = new TaskResponse(
+                createdTask.getId(),
+                createdTask.getTitle(),
+                createdTask.getDescription(),
+                createdTask.getStatus().name(),
+                createdTask.getDueDate()
+        );
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}
