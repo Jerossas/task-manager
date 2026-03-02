4af0c12b5718124b9e8afc87a8a02826dd7f1431
Jerozzzzzzzzzzzz <jerossas@outlook.com>
2026-03-01 21:02:09 -0500

    feature(web): add use cases bean configuration for dependency injection

---
+++ web/src/main/java/com/dunnwr/cleantaskmanager/config/UseCaseConfig.java
@@ -0,0 +1,15 @@
+package com.dunnwr.cleantaskmanager.config;
+
+import com.dunnwr.cleantaskmanager.domain.gateways.TaskRepository;
+import com.dunnwr.cleantaskmanager.usecases.CreateTaskUseCase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class UseCaseConfig {
+
+    @Bean
+    public CreateTaskUseCase createTaskUseCase(TaskRepository taskRepositoryAdapter){
+        return new CreateTaskUseCase(taskRepositoryAdapter);
+    }
+}
