f37d84df36cebf77e8353090a4642aaa3d731d5b
Jerozzzzzzzzzzzz <jerossas@outlook.com>
2026-03-01 21:09:50 -0500

    feat(web): create main class for execution

---
+++ web/src/main/java/com/dunnwr/cleantaskmanager/CleanTaskManagerApplication.java
@@ -0,0 +1,12 @@
+package com.dunnwr.cleantaskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanTaskManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanTaskManagerApplication.class, args);
+    }
+}
